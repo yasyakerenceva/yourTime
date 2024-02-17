b963e2b94193fc3ce73ff04583c0d04398fe7e4e
Yaroslava Kerenceva <pyi.work@yandex.ru>
2024-02-17 14:52:07 +0700

    Подключение стилей, шрифтов, фавиконки

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
 	content: ["./src/**/*.{html,js,jsx}"],
 	theme: {
+		fontFamily: {
+			montserrat: ["Montserrat", "sans-serif"],
+			"rock-salt": ["RockSalt", "sans-serif"],
+		},
 		extend: {},
 	},
 	plugins: [],
